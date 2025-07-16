d9d0ed3dd89c6677c00cde49d7a0141b4daa700c
Sameer-Behera365 <sonubehera2024@gmail.com>
2025-07-16 15:25:42 +0530

    commit 3

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,7 +10,28 @@ import React, { useState } from 'react'
 
 //ok look navbar has 3 columns   1st column is logo  and 2nd column is menu items   and 3rd column is search icon and login button
 const Navbar = () => {
-  return (
+
+const [isOpen, setIsOpen] = useState(false)     //initially false 
+
+/*
+use of the isOpen  and setIsOpen:--->
+Variable 	               Purpose
+
+isOpen	                 Checks whether the mobile nav is open
+setIsOpen                Toggles open/close when clicking icons
+MenuIcon	               Opens the menu (isOpen = true)
+XIcon	                   Closes the menu (isOpen = false)
+
+📱 On small screens, clicking the MenuIcon (☰) → sets isOpen = true → menu opens.
+❌ Clicking the XIcon → sets isOpen = false → menu closes.
+
+*/
+
+
+
+
+
+return (
 <div className='fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5'>
 
 {/*----------------------- 1st column------------------------------ */}.
@@ -48,24 +69,28 @@ const Navbar = () => {
 
 {/*---------------------- 2nd column------------------------------ */}
 
-<div>
+      <div className={`max-md:absolute max-md:top-0 max-md:left-0 max-md:font-medium max-md:text-lg z-50 flex flex-col md:flex-row items-center max-md:justify-center gap-8 min-md:px-8 py-3 max-md:h-screen min-md:rounded-full backdrop-blur bg-black/70 md:bg-white/10 md:border border-gray-300/20 overflow-hidden transition-[width] duration-300 ${isOpen ? 'max-md:w-full' : 'max-md:w-0'}`}>    {/* explained  in docs */}
 
-</div>
+        <XIcon className='md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer' onClick={()=> setIsOpen(!isOpen)}/>   {/* only show in small screens and Hide on medium and larger screens (≥768px)   ,   absolute: Position the icon independently inside its container.    ,  24px down from the top (top-6)     and   24px in from the right (right-6)    So it appears in the top-right corner, often used to close mobile menus or sidebars.    ,   Width and height = 1.5rem (24px)    ,    When you hover, the mouse becomes a hand icon, meaning it’s clickable.    */}          {/*  shows a close (X) icon — used to close menus, modals, or sidebars. */}
+
+        <Link onClick={()=> {scrollTo(0,0); setIsOpen(false)}} to='/'>Home</Link>
+        <Link onClick={()=> {scrollTo(0,0); setIsOpen(false)}} to='/movies'>Movies</Link>
+        <Link onClick={()=> {scrollTo(0,0); setIsOpen(false)}} to='/'>Theaters</Link>
+        <Link onClick={()=> {scrollTo(0,0); setIsOpen(false)}} to='/'>Releases</Link>
+        <Link onClick={()=> {scrollTo(0,0); setIsOpen(false)}} to='/favorite'>Favorites</Link>
+
+      </div>  
 
 
-<div className='flex items-center gap-8'>
+
+{/* -----------------3rd column----------------------------- */}
+
+
+
+<div className='flex items-center gap-8'>            {/* ➡️ Adds space between the child elements inside the flex row.   gap-8 = 2rem = 32px of spacing between each item. */}
   <SearchIcon className='max-md:hidden w-6 h-6 cursor-pointer'/>      {/*hidden on small screen and width and height is 1.5rem also on hover we get hand as the cursor  */}
-        {
-            !user ? (
-                <button onClick={openSignIn} className='px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer'>Login</button>
-            ) : (
-                <UserButton>
-                    <UserButton.MenuItems>
-                        <UserButton.Action label="My Bookings" labelIcon={<TicketPlus width={15}/>} onClick={()=> navigate('/my-bookings')}/>
-                    </UserButton.MenuItems>
-                </UserButton>
-            )
-        }
+  <button  className='px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer'>Login</button>      {/* login button */}
+          
         
 </div>
 
@@ -74,11 +99,14 @@ const Navbar = () => {
 
 
 
-{/*   after all teh three columns we will add this icon   (☰)   */}
+{/*   after all teh three columns we will add this icon   (☰)  in samller screens */}
 <MenuIcon className='max-md:ml-4 md:hidden w-8 h-8 cursor-pointer' onClick={()=> setIsOpen(!isOpen)}/>
-{/* 
 
-🔹 max-md:ml-4
+
+
+{
+/* 
+max-md:ml-4:----->
 max-md: means the style applies on small screens only (screen ≤ 768px).
 
 ml-4 = margin-left: 1rem (16px).
@@ -98,10 +126,8 @@ Width and height set to 2rem (32px) each.
 Changes mouse cursor to a pointer (hand icon) on hover.
 📌 Means: “This element is clickable.”
 
-*/}
-
-
-
+*/
+}
 
 
 </div>    //end of the entire div container 
